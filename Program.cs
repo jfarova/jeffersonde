c2540dda1c30755faf080f819d108929af8b98c3
chenaoh <jfarova112333@gmail.com>
2022-11-20 17:04:50 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n1;
+       Console.WriteLine("digite el primer numero");
+       n1=int.Parse(Console.ReadLine());
+       
+       if(n1% 2 == 0){
+        Console.WriteLine("el numero  es par" + n1);
+
+
+       }
+       else{
+        Console.WriteLine("el numero  es impar" + n1);
+       }
+    }
+    }
